deea649e265348ce38fb5ff4725c46985b8b60cd
Sevim Cengiz <sevim_cengiz@icloud.com>
2022-04-19 21:22:31 +0400

    Update build script with cd

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 set -ev
 
 export RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/quarto/bin"
-Rscript -e "bookdown::render_book('docs/index.Rmd', 'bookdown::gitbook', output_dir = 'book')"
+cd docs
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', output_dir = '../book')"
