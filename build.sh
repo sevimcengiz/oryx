b7cd9db1c7330f9be6f034f24f9e9f130cfdd7ed
Sevim Cengiz <sevim_cengiz@icloud.com>
2022-04-20 21:02:27 +0400

    Add git push to build script

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ set -ev
 export RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/quarto/bin"
 cd docs
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', output_dir = '../book')"
+cd ..
+git push
